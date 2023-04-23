71a1fac98759b543d4b8f025632f6254e3f73a2c
zderApfel <scoutt172@yahoo.com>
2023-04-23 12:26:38 -0400

    ALL MY CONFIGS DISAPPEARED NOO

--- kubejs/client_scripts/renameItems.js
+++ kubejs/client_scripts/renameItems.js
@@ -4,11 +4,4 @@ onEvent('client.generate_assets', event => {
 	const rename = (item, newName) => event.addLang(Item.of(item).item.getDescriptionId(), newName)
 	
 	rename('beyond_earth:compressed_steel','Steel Plate')
-	rename('nzgexpansion:hunting_rifle', 'Wheatfield 700')
-	rename('nzgexpansion:hunting_shotgun', 'Bs-533')
-	rename('nzgexpansion:revolver','V&L M469')
-	rename('cgm:basic_bullet','Pistol round')
-	rename('nzgexpansion:medium_bullet','Mid-Power Round')
-	rename('cgm:shell','12-guage shell')
-	rename('cgm:advanced_bullet','Full-Power Round')
 })
--- kubejs/server_scripts/recipe.js
+++ kubejs/server_scripts/recipe.js
@@ -126,6 +126,3 @@ shapeless.forEach(x => {
 	})
 	
 })
-
-
-
